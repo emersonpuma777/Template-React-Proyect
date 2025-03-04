0129cb6c6792a6f9772333fc76ef5c170dffcaf3
emersonpumaat <emerson@accounttech.com>
2025-03-04 16:01:32 -0500

    Thrid commit

--- src/infrastructure/models/specialty.ts
+++ src/infrastructure/models/specialty.ts
@@ -1,12 +1,12 @@
 import { z } from "zod";
 
-const doctorParser = z.object({
+const specialtyParser = z.object({
   id: z.string().nullable(),
   name: z.string().nullable(),
   label: z.string().nullable(),
   hourly_rate: z.string().nullable(),
 });
 
-export type DoctorParser = z.infer<typeof doctorParser>;
+export type specialtyParser = z.infer<typeof specialtyParser>;
 
-export default doctorParser;
+export default specialtyParser;
